b6bd3132c110bdacef185e4a23d938abc5ab6f51
Abdulrahman HADJADJ <59369187+GenDexx@users.noreply.github.com>
2025-06-03 03:06:05 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 module.exports = {
   TOKEN: "",
   language: "en",
-  ownerID: ["557401286472105986"], 
+  ownerID: ["557401286472105986","700144576882671696"], 
   mongodbUri : "mongodb+srv://GenDex:nKsxuHlieeUmZouo@discordbot.hwkt4pz.mongodb.net/?retryWrites=true&w=majority",
   spotifyClientId : "9b7aa9c200f74cd38977379d6f14ee92",
   spotifyClientSecret : "5c1347ece43d4215bb3f418bd20abffc",
